d890cf2430d06668e4a1b4b3d2e035ed2ffd4518
danielOliveira97 <34692157+danielOliveira97@users.noreply.github.com>
2023-03-08 18:32:04 -0300

    Update change_dynamodb_capacity_units.py

--- python-scripts/change_dynamodb_capacity_units.py
+++ python-scripts/change_dynamodb_capacity_units.py
@@ -8,22 +8,16 @@ table_list = dynamodb.list_tables()['TableNames']
 
 # Loop through each table and update the provisioned capacity units
 for table_name in table_list:
-    # Get the current provisioned capacity units for the table
-    current_provisioning = dynamodb.describe_table(TableName=table_name)['Table']['ProvisionedThroughput']
-    current_read_capacity = current_provisioning['ReadCapacityUnits']
-    current_write_capacity = current_provisioning['WriteCapacityUnits']
+    # Get the billing mode for the table
+    billing_mode = dynamodb.describe_table(TableName=table_name)['Table']['BillingModeSummary']['BillingMode']
     
-    # Calculate the desired new provisioned capacity units
-    new_read_capacity = current_read_capacity * 2
-    new_write_capacity = current_write_capacity * 2
-    
-    # Update the provisioned capacity units for the table
-    dynamodb.update_table(
-        TableName=table_name,
-        ProvisionedThroughput={
-            'ReadCapacityUnits': new_read_capacity,
-            'WriteCapacityUnits': new_write_capacity
-        }
-    )
-    
-    print(f"Updated provisioned capacity units for table '{table_name}' to {new_read_capacity} read capacity units and {new_write_capacity} write capacity units")
+    if billing_mode == 'PAY_PER_REQUEST':
+        # Update the provisioned capacity units for the table
+        dynamodb.update_table(
+            TableName=table_name,
+            BillingMode='PAY_PER_REQUEST'
+        )
+        
+        print(f"Updated billing mode for table '{table_name}' to PAY_PER_REQUEST")
+    else:
+        print(f"Table '{table_name}' is already using billing mode {billing_mode}, skipping update")
